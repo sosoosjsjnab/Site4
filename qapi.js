26022640b11e1cf879330bf847b4ebace8dc57ad
sosoosjsjnab <74925201+sosoosjsjnab@users.noreply.github.com>
2020-11-24 22:23:34 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,55 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbd5b2d8996cc001edd34f6",
+				"5fbd5b2d8996cc001edd34f8",
+				"5fbd5d1db83a2f001f2e30e7",
+				"5fbd5d6679530b001fd2bfc0",
+				"5fbd5d8079530b001fd2bfc2"
+			]
+		},
+		"5fbd5b2d8996cc001edd34f6": {
+			"id": "5fbd5b2d8996cc001edd34f6",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbd5b2d8996cc001edd34f8": {
+			"id": "5fbd5b2d8996cc001edd34f8",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z"
+			}
+		},
+		"5fbd5d1db83a2f001f2e30e7": {
+			"id": "5fbd5d1db83a2f001f2e30e7",
+			"pageUrl": "terms",
+			"name": "Terms And Conditions",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z"
+			}
+		},
+		"5fbd5d6679530b001fd2bfc0": {
+			"id": "5fbd5d6679530b001fd2bfc0",
+			"pageUrl": "privacy",
+			"name": "Privacy Policy",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z"
+			}
+		},
+		"5fbd5d8079530b001fd2bfc2": {
+			"id": "5fbd5d8079530b001fd2bfc2",
+			"pageUrl": "cookies",
+			"name": "Cookies",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z"
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,211 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Icon, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Section, Override, StackItem, Stack } from "@quarkly/components";
+import { MdDone } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/stroychik.ru__1565962264.jpg?v=2020-11-24T19:18:28.801Z) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					How to adjust plastic windows
+				</Text>
+				<Text font="--base">
+					Plastic window systems are installed in almost every modern home and office space. They are preferred for their versatility, practicality and durability. However, situations often occur when the window block ceases to perform its functions or requires urgent repair.{"\n\n"}
+				</Text>
+			</Box>
+		</Section>
+		<Section color="--dark">
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdDone}
+				/>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					{" "}In addition, not every owner of PVC structures knows that they need to be periodically adjusted. Because of this, a lot of difficulties arise. Today there are many firms providing services for the repair and adjustment of windows. However, regularly contacting them for help will require material costs from you. Knowing how to adjust plastic windows, you can do all the work yourself. This will save you money and extend the life of the entire unit. What you need to know about adjusting the window and how to troubleshoot the problems that arise, read our article. In what situations is it necessary to adjust the unit After installing the plastic window, a warm and comfortable microclimate is established in the room. However, after a while, cold can begin to blow from the opening, a draft appears. This may be due to the shrinkage of the building or the weakening of the sash pressure. In this case, system repair is required. Knowing how to adjust plastic windows, you can solve a number of other problems. Namely: eliminate the sagging of the moving part; eliminate drafts; improve heat and sound insulation of the window; set up the work of the PVC system fittings; prepare the block for the change of seasons. During the operation of the window, the sash begins to rub against the frame, the handles can become loose and stop scrolling altogether. In such a situation, the further functioning of the system will be difficult. Don't panic, however. Fixing these problems is easy enough. How to do this, read below.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdDone}
+				/>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					{" "}Prepare the necessary tools before starting work. If you are wondering how to adjust plastic windows yourself, you must have the appropriate hand tools. The list of necessary accessories includes: Hex keys (4 mm type is especially important); flat and star screwdriver; pliers; lubricants (for example, machine oil or WD-40 spray). In the process of performing most of the work on setting up a PVC window, you will need a furniture key (aka a hexagon). Work with fittings is done using a screwdriver with various attachments. This work does not require a lot of time. A typical window setting is done in 20-25 minutes. Elimination of defects and replacement of fittings will last no more than an hour. What you need to know before starting work There is a lot of information on the Internet on how to adjust plastic windows for winter mode, eliminate possible problems and adjust the entire system. However, the incomprehensible terms in them enter into a stupor of the ordinary reader. How to do the work yourself, if you don't understand what the instructions are about? Such a reaction is caused by the names of the main device that ensures the locking of the sash - the pin, the eccentric. To understand what this is about, open the sash of your window. From its end you will see metal elements resembling a cylinder (or oval). Most often they have special holes for adjustment with a furniture key. It is about these elements that we are talking about. They are located in the center of the frame, below and above it. To adjust the plastic window for the winter (as well as for the summer), you need to work not only in the area of ​​eccentrics, but also in the locations of the hinges. For this, the protective caps are first removed.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdDone}
+				/>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					How to prepare the PVC system for the change of seasons During the installation of the plastic window unit, a middle clamping of the sash to the frame is set. With the onset of the cold season, the tightness of the tear-off part should be maximum. This avoids the penetration of cold air into the room. In the summer, the pressure should be loosened. This approach helps to ensure a favorable microclimate in the room. In addition, the weakening of the load on the sealing material contributes to a longer service life. To adjust plastic windows for winter mode as correctly as possible, carry out the work in strict accordance with the instructions. It involves the following actions: Open the sash and locate all the eccentrics. There are 4-5 of them on a standard frame. If we are talking about euro doors, then the number of trunnions can reach 9 pieces. If the pressure elements do not have a hole for a hexagon, then they must be adjusted with pliers. In all other cases, use a furniture key. Pay attention to the location of the eccentrics. If they stand vertically, the window is in summer mode, and if horizontally - in winter mode. Turn the pins clockwise to the desired position. In some systems, to adjust the pressure element, it must be pulled towards you and only then turned. Adjust all pressure pins. If you miss a few of them, closing the window will damage the integrity and evenness of the block. Check if the frame is tight enough. To do this, insert a sheet of paper between the frame and the window, close the movable part of the block. If the sheet is difficult to reach, the system is well adjusted. If easy, replace the sealing gum. If you figured out how to adjust the plastic window for the winter, preparing the unit for the summer will no longer raise any questions. Work is carried out in the same way, only the eccentrics are installed in a vertical position.
+				</Text>
+			</Box>
+		</Section>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2331108.jpg?v=2020-11-24T19:18:28.795Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/12cafc20a148a04a67542250eccdeea4.jpg?v=2020-11-24T19:18:28.792Z" width="320px" max-width="100%" />
+				</Box>
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/furnitura_30-5.jpg?v=2020-11-24T19:18:28.789Z" width="320px" max-width="100%" />
+				</Box>
+			</Box>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--base" margin="20px 0 0 0">
+				What to do if blowing from the window? If the temperature drops in the room during the onset of cold weather, there is a possibility that cold air passes through the window. Use regular matches to detect drafts. Light one of them and bring it to the window. If the fire is directed in one direction, the penetration of cold air takes place. How to adjust a plastic window so that it doesn't blow? First, increase the frame clamp. Turn all eccentrics clockwise. Adjustment of the degree of pressure varies within 2 mm. If after the work done, the thrust remains, replace the seal. Measure the frame perimeter and determine how much seal you need. Get expensive and high quality options. They have a longer service life. In addition to the rubber seal, you will need special glue. It must be applied to the groove around the frame. Then install the rubber band and close the window while the adhesive dries. If the frame starts to close very tightly, you will need to adjust the plastic windows. How to do this, read above. What to do if large gaps are detected If there are large gaps between the frame and the moving part of the window, then the adjustment must be carried out using the hinges. To understand how to adjust the plastic windows on the hinge side, you need to remove the decorative caps from them. First, attention should be paid to the upper suspension of the window. It can be used to adjust the horizontal offset of the top of the frame. To do this, take the L-shaped furniture key and position the short part of it in the adjustment hole. It is located at the end of the sash itself. Perform work with an open window. Turn the screw slightly and check the ease of closing the window. This way you can move the upper part of the frame from -2 to +2 mm. The second method, how to adjust the sash of a plastic window, is the alignment from the side of the lower hinge. Work is carried out in the event that the frame is strongly skewed, and the window becomes difficult to close. This happens most often in new homes. In the process of adjustment, the same L-shaped hexagon is used. How to adjust a plastic window so that it closes as easily as possible? To do this, proceed as follows: With the window open, insert the hexagon into the adjustment hole. Turn the screw in the desired direction. Check the position of the lower window strip. Adjust the vertical offset of the entire sash. Open the window slightly. Insert the long section of the keyhole into the hole at the top of the hinge. Scroll with the key in the desired direction. Check the correct position of the sash. Using this method, you can adjust the vertical and horizontal position of the opening part of the frame in the range from -2 to +2 mm.
+			</Text>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/1a4592499edf27832896973e50ffcc3a844bfec15bb6b5abc2eb7dbd9d193673.jpg?v=2020-11-24T19:18:28.786Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0" />
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--base" margin="20px 0 0 0">
+				Summing up We figured out how to adjust the plastic windows yourself. As you can see, this process is more than simple, does not require special skills and the presence of a professional tool. Any homeowner can perform maintenance work on PVC systems. And in order to quickly identify the problem and find a method to solve it, you just need to have the appropriate instructions at hand. Seasonal window adjustments should be done twice a year. Then your window will serve you as long as possible. If large gaps appear between the sash and the unit, perform work to eliminate them. To do this, adjust the sash vertically and horizontally using the holes in the canopies. Similar work must be performed in the following cases: if the sash rubs against the upper part of the frame, you need to open the window and push it forward on the upper hinge with the short side of the furniture key; if the sash sags and clings to the bottom of the frame, you need to adjust it through the hole in the lower hinge; if it rubs against the side of the frame, push it in the opposite direction with the short side of the L-shaped key on both hinges. The same work is performed in cases where the plastic window does not close well. How to adjust the sash if there is a draft?
+			</Text>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Image width="450px" height="450px" src="https://uploads.quarkly.io/5fbd5b2d8996cc001edd34f3/images/2273.jpg?v=2020-11-24T19:18:28.786Z" />
+		</Section>
+		<Section
+			text-align="center"
+			background-color="--primary"
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+		>
+			<Text as="p" font="--lead" margin="40px 0 20px 0">
+				By adjusting all the eccentrics. If the work done did not prevent the penetration of cold air into the room, replace the seal. However, remember that if you replaced the rubber band, you need to loosen the sash to the frame. Otherwise, the window will close tightly, and the sealing material itself will quickly fail. Perform window maintenance work carefully and slowly. Remember to change the position of all pins at the same time. If more difficult situations arise, seek professional help. Our article will help you solve common problems.{"\n\n"}
+			</Text>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" color="--dark" background="rgba(0, 0, 0, 0.06)">
+			<Stack>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						as="p"
+						font="--base"
+						margin="12px 0"
+						color="--greyD2"
+						text-align="center"
+					>
+						<Link href="#">
+							Terms And Conditions
+						</Link>
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						as="p"
+						font="--base"
+						margin="12px 0"
+						color="--greyD2"
+						text-align="center"
+					>
+						<Link href="#">
+							Privacy Policy
+						</Link>
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						as="p"
+						font="--base"
+						margin="12px 0"
+						color="--greyD2"
+						text-align="center"
+					>
+						<Link href="#">
+							Cookies
+						</Link>
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
